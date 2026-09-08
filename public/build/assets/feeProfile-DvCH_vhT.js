5fa92a5000101ea6cef5ecb953c7af0bcac48d4f
Developer <dev@school.com>
2026-09-08 15:47:36 +0530

    build: compile fresh production assets after merging harsh

---
+++ public/build/assets/feeProfile-DvCH_vhT.js
@@ -0,0 +1,1 @@
+const e={all:["fee-profiles"],list:l=>[...e.all,"list",l],detail:l=>[...e.all,"detail",l],profileDetail:l=>[...e.all,"profile-detail",l]};export{e as F};
---
+++ public/build/assets/subMonths-DT_Jv7_B.js
@@ -0,0 +1,1 @@
+import{a as n}from"./addMonths-DxcpM2BV.js";function r(o,s,t){return n(o,-1,t)}export{r as s};
